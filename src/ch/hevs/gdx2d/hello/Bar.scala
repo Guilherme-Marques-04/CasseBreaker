af3e54c94309881e186f39325ca3589d4f920785
Julien Rdt <jujurdt@gmail.com>
2025-05-29 14:24:22 +0200

    Ajout déplacement de la barre

--- src/ch/hevs/gdx2d/hello/Bar.scala
+++ src/ch/hevs/gdx2d/hello/Bar.scala
@@ -1,17 +1,38 @@
 package ch.hevs.gdx2d.hello
 
 import ch.hevs.gdx2d.lib.GdxGraphics
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Color
 
-class Bar(var posX: Int, var posY: Int, var size:Int) extends Drawable {
+class Bar(var posX: Int, var posY: Int, var height: Int, var width: Int, var color: Color) extends Drawable {
+  val speed: Int = 20
+  var moveLeft: Boolean = false
+  var moveRight: Boolean = false
+
+  def updateBar(): Unit = {
+    if (moveLeft && posX > 0 + width/2)
+      posX -= speed
+    else if (moveRight && posX < 1920 - width/2)
+      posX += speed
+  }
+
+  def onKeyDown(keycode: Int): Unit = {
+    keycode match {
+      case Input.Keys.LEFT => moveLeft = true
+      case Input.Keys.RIGHT => moveRight = true
+      case _ =>
+    }
+  }
 
   def onKeyUp(keycode: Int): Unit = {
-
+    keycode match {
+      case Input.Keys.LEFT => moveLeft = false
+      case Input.Keys.RIGHT => moveRight = false
+      case _ =>
+    }
   }
 
   override def draw(g: GdxGraphics): Unit = {
-
-    g.drawFilledRectangle(posX,posY, size, 90, 90)
-
+    g.drawFilledRectangle(posX , posY, height, width, 90, color)
   }
 }
-
--- src/ch/hevs/gdx2d/hello/Game.scala
+++ src/ch/hevs/gdx2d/hello/Game.scala
@@ -1,26 +1,40 @@
 package ch.hevs.gdx2d.hello
 
-import ch.hevs.gdx2d.lib.GdxGraphics
 import ch.hevs.gdx2d.desktop.PortableApplication
+import ch.hevs.gdx2d.lib.GdxGraphics
+import com.badlogic.gdx.InputAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 
 object Game {
-
   def main(args: Array[String]): Unit = {
     new Game
   }
 }
 
-class Game extends PortableApplication(1920,1080) {
+class Game extends PortableApplication(1920, 1080) {
 
-  val bar: Bar = new Bar(300,300,10)
+  val bar = new Bar(800, 100, 20, 250, Color.WHITE)
 
   override def onInit(): Unit = {
+    Gdx.input.setInputProcessor(new InputAdapter {
+      override def keyDown(keycode: Int): Boolean = {
+        bar.onKeyDown(keycode)
+        true
+      }
 
+      override def keyUp(keycode: Int): Boolean = {
+        bar.onKeyUp(keycode)
+        true
+      }
+    })
   }
 
   override def onGraphicRender(g: GdxGraphics): Unit = {
     g.clear()
     g.drawFPS()
+
+    bar.updateBar()
     bar.draw(g)
   }
 }
